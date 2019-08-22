96c7481eef638375cb4daf0bac6410236982aae1
andreas.dominik <andreas.dominik@mni.thm.de>
2019-08-22 08:10:46 +0200

    off with timer

--- Skill/api.jl
+++ Skill/api.jl
@@ -11,17 +11,17 @@ end
 function spawnIrrigation(ip, duration, repeats)
 
     Snips.printDebug("spawnIrrigation() started")
-    
+
     sleeptime = duration * 60
     for i in (1:repeats)
-        if Snips.switchShelly1(ip, :on)
+        if Snips.switchShelly1(ip, :timer; timer = sleeptime)
             Snips.printLog("Irrigation started for $i of $repeats repeats")
         else
             Snips.printLog("cloud not start irrigation with device at $ip")
             Snips.publishSay(:error_on)
         end
         sleep(sleeptime)
-        Snips.switchShelly1(ip, :off)
+        # Snips.switchShelly1(ip, :off)
         Snips.printLog("Irrigation stopped for $i of $repeats repeats")
         sleep(sleeptime)
     end
